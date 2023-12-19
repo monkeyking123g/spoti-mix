6a42ebbb4224232ad82f679e8efcbaee24401b18
Dmytro Ipatii <87619572+monkeyking123g@users.noreply.github.com>
2023-12-19 16:54:32 +0000

    add card

--- app/tracks/page.tsx
+++ app/tracks/page.tsx
@@ -1,9 +1,47 @@
-import { title } from "@/components/primitives";
-
+"use client";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { Button } from "@nextui-org/button";
 export default function AboutPage() {
-  return (
-    <div>
-      <h1 className={title()}>Tracks</h1>
-    </div>
-  );
+  const tracks = [1, 2, 4, 5, 5, 6, 1, 2, 4, 5, 5, 6];
+  return tracks.map(() => {
+    return (
+      <Card className="py-4">
+        <CardHeader className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="https://source.unsplash.com/random/200x200?sig=1"
+            width={270}
+          />
+        </CardHeader>
+        <CardBody className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          <small className="text-default-500">12 Tracks</small>
+          <h4 className="font-bold text-large">Frontend Radio</h4>
+        </CardBody>
+        <CardFooter className="flex justify-between">
+          <Button
+            className=" text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
+          >
+            Play
+          </Button>
+
+          <Button
+            className="text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
+          >
+            Pause
+          </Button>
+        </CardFooter>
+      </Card>
+    );
+  });
 }
